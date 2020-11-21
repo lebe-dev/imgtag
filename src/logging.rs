076982c5e16f6896e6ff22dc9f8f14a351c6fbc1
Eugene Lebedev <duke.tougu@gmail.com>
2020-11-21 19:17:29 +0300

    [+] project skeleton

---
+++ src/logging.rs
@@ -0,0 +1,55 @@
+pub mod logging {
+    use log4rs::append::file::FileAppender;
+    use log4rs::config::{Appender, Config, Logger, Root};
+    use log4rs::encode::pattern::PatternEncoder;
+    use log::LevelFilter;
+
+    const FILE_APPENDER_NAME: &str = "file";
+
+    pub fn get_logging_config(logging_level: &str) -> Config {
+        let level = get_logging_level_from_string(logging_level);
+
+        Config::builder()
+            .appender(get_file_appender_definition())
+            .logger(get_default_logger())
+            .build(
+            Root::builder()
+                .appender(FILE_APPENDER_NAME)
+                .build(level)
+            )
+            .unwrap()
+    }
+
+    fn get_logging_level_from_string(level: &str) -> LevelFilter {
+        return match level {
+            "debug" => LevelFilter::Debug,
+            "error" => LevelFilter::Error,
+            "warn" => LevelFilter::Warn,
+            "trace" => LevelFilter::Trace,
+            "off" => LevelFilter::Off,
+            _ => LevelFilter::Info
+        };
+    }
+
+    fn get_file_appender_definition() -> Appender {
+        Appender::builder()
+            .build(FILE_APPENDER_NAME, Box::new(get_file_appender())
+        )
+    }
+
+    fn get_file_appender() -> FileAppender {
+        FileAppender::builder()
+            .encoder(get_encoder())
+            .build("imgtag.log")
+            .unwrap()
+    }
+
+    fn get_encoder() -> Box<PatternEncoder> {
+        Box::new(PatternEncoder::new("{d(%Y-%m-%d %H:%M:%S)} - {l} - {m}{n}"))
+    }
+
+    fn get_default_logger() -> Logger {
+        Logger::builder()
+                .build("default", LevelFilter::Info)
+    }
+}
