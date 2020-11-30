0d5b4aaf73987068d39e67c21e3323a54d63e002
Eugene Lebedev <duke.tougu@gmail.com>
2020-11-30 13:35:54 +0300

    [~] bump app version to 0.4.0

--- src/main.rs
+++ src/main.rs
@@ -58,7 +58,7 @@ fn main() {
         .required(false);
 
     let matches = App::new("imgtag")
-        .version("0.3.0")
+        .version("0.4.0")
         .about("Image files hierarchy tool")
         .arg(
             Arg::with_name(LOG_LEVEL_ARGUMENT)
